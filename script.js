ccb57d75b7ecbe4b8196faaaf2ce319d878ee958
muhammadaziz <ergashevmuhammadaziz03@gmail.com>
2023-12-03 21:14:26 +0500

    ok

---
+++ script.js
@@ -0,0 +1,11 @@
+function openSidebar() {
+    navbar = document.getElementById("navbar");
+
+    navbar.style.right = "0";
+}
+
+function hideSidebar() {
+    navbar = document.getElementById("navbar");
+
+    navbar.style.right = "-65%";
+}
